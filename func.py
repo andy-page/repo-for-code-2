d3330166b54dd5c5e92588d7f3674570ad49ca41
Aadil Shaikh <aadil.nawaz.shaikh@oracle.com>
2022-05-12 10:12:04 -0500

    another commit

--- func.py
+++ func.py
@@ -6,7 +6,7 @@ from fdk import response
 
 
 def handler(ctx, data: io.BytesIO=None):
-    name = "hello World"
+    name = "hello World New"
     try:
         body = json.loads(data.getvalue())
         name = body.get("name")
